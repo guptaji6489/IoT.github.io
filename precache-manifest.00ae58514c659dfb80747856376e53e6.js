52bef49301fb2f47244e4b902018ec23076f0bba
guptaji6489 <guptaji6489@gmail.com>
2020-07-08 13:53:18 +0530

    uploaded

---
+++ precache-manifest.00ae58514c659dfb80747856376e53e6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07040b06d798e172c39c2ddc6f955420",
+    "url": "/My-First-ReactJS-Website/index.html"
+  },
+  {
+    "revision": "aeed33ee398c6ef2a24e",
+    "url": "/My-First-ReactJS-Website/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "253c34dc2a55d62b3bc5",
+    "url": "/My-First-ReactJS-Website/static/css/main.3fe2f89c.chunk.css"
+  },
+  {
+    "revision": "aeed33ee398c6ef2a24e",
+    "url": "/My-First-ReactJS-Website/static/js/2.59da76ad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/My-First-ReactJS-Website/static/js/2.59da76ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "253c34dc2a55d62b3bc5",
+    "url": "/My-First-ReactJS-Website/static/js/main.d4a3da25.chunk.js"
+  },
+  {
+    "revision": "7f884fe8b6fde991d861",
+    "url": "/My-First-ReactJS-Website/static/js/runtime-main.169b8115.js"
+  },
+  {
+    "revision": "59a4799f45d50e66fd96d05986fb06bb",
+    "url": "/My-First-ReactJS-Website/static/media/1.59a4799f.jpg"
+  }
+]);
